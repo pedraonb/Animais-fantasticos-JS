d39a168262e419d86de83999ff689d8904992fb8
pedraonb <pedraonb@outlook.com>
2021-07-08 10:21:13 -0300

    seleção de elementos

---
+++ script.js
@@ -0,0 +1,14 @@
+const animais = document.getElementById('animais');
+console.log(animais);
+
+const gridSection = document.getElementsByClassName('grid-section');
+console.log(gridSection);
+
+const primeiraLi = document.querySelector('li');
+console.log(primeiraLi);
+
+const primeiraUl = document.querySelector('ul');
+console.log(primeiraUl);
+
+const linkInterno = document.querySelector('[href^="#"]');
+console.log(linkInterno);
